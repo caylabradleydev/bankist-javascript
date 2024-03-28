8dcda452f9f409b4686cd1d84b135e58fdfd9cba
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-28 11:16:40 -0500

    delete account functionality

--- script.js
+++ script.js
@@ -171,6 +171,24 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+  }
+
+  inputClosePin.value = inputCloseUsername.value = '';
+});
+
 // const firstWithdrawal = movements.find(movement => movement < 0);
 
 // console.log(firstWithdrawal); //-400
