f2523823117a01c4cca2beeb8c3c1022f58f9dc9
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-28 11:24:47 -0500

    request loan functionality

--- script.js
+++ script.js
@@ -171,6 +171,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov > amount * 0.1)) {
+    currentAccount.movements.push(ammount);
+    updateUI();
+  }
+
+  inputLoanAmount = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
