17c15d930daf5b812cf9f0a9430c6138c69c47af
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-30 10:49:26 -0500

    implement page navigation

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,12 +32,19 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
-
+//Button Smooth Scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coordinates = section1.getBoundingClientRect();
 
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page Navigation
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
