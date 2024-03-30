a62f59a9d7cbf7e74532ed8f3a5e4d7ffd13499a
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-30 11:43:39 -0500

    implement nav hover

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -50,10 +54,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 //Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const btnClicked = e.target.closest('.operations__tab');
 
@@ -68,3 +68,20 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${btnClicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+const handleNavHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(element => {
+      if (element !== link) element.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleNavHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleNavHover.bind(1));
